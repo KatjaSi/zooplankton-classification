9df3f85fdb04d42994cd73d6e4c6fd5af517e84a
KatjaSi <katja.sivertsen@gmail.com>
2024-05-22 08:48:45 +0200

    train_utils.py

--- src/train.py
+++ src/train.py
@@ -21,59 +21,7 @@ import pandas as pd
 import copy
 
 
-def one_iter(loader, device, train=True, optimizer=None, scheduler=None, monitoring_metrics=list()):
-    running_loss = 0.0
-    count = 0.0
-    if train:
-        model.train()
-    else:
-        model.eval()
-
-    preds_arr = []
-    targets_arr = []
-    for data, labels in (loader):
-        batch_size = len(labels)
-        data = data.to(device)
-        labels = labels.to(device)
-        if optimizer is not None:
-            optimizer.zero_grad()
-        outputs = model(data)
-        loss = criterion(outputs, labels)
-        if train:
-            loss.backward()
-            optimizer.step()
-
-        preds = outputs.max(dim=1)[1]
-        count += batch_size
-        running_loss += loss.item() * batch_size
-
-        targets_arr.append(labels.cpu().numpy())
-        preds_arr.append(preds.detach().cpu().numpy())
-
-        if train and scheduler is not None:
-            scheduler.step()
-
-    targets = np.concatenate(targets_arr)
-    preds = np.concatenate(preds_arr)
-
-    loss = running_loss*1.0/count
-
-    result = {"loss": loss}
-    if "accuracy" in monitoring_metrics:
-        accuracy = metrics.accuracy_score(targets, preds)
-        result["accuracy"] = accuracy
-    if "balanced_accuracy" in monitoring_metrics:
-        balanced_accuracy = metrics.balanced_accuracy_score(targets, preds)
-        result["balanced_accuracy"] = balanced_accuracy
-    if "confusion_matrix" in monitoring_metrics:
-        cm = metrics.confusion_matrix(targets, preds)
-        result["confusion_matrix"] = cm
-    if "recall_per_class" in monitoring_metrics:
-        recall_per_class = metrics.recall_score(targets, preds, average=None)
-        result["recall_per_class"] = recall_per_class
-
-    return result
-
+from train_utils import one_iter
 
 
 if __name__ == "__main__":
@@ -166,7 +114,7 @@ if __name__ == "__main__":
     num_epochs = max_num_epochs
     patience_count = patience
     for epoch in range(max_num_epochs):  
-        result = one_iter(train_loader, 
+        result = one_iter(model, criterion, train_loader, 
                             device, 
                             train=True, 
                             optimizer=optimizer,
@@ -191,7 +139,7 @@ if __name__ == "__main__":
                 ] if enabled
             ]
         
-        result = one_iter(val_loader, 
+        result = one_iter(model, criterion, val_loader, 
                                 device, 
                                 train=False, 
                                 monitoring_metrics=monitoring_metrics)
@@ -236,7 +184,7 @@ if __name__ == "__main__":
     # test
     model.load_state_dict(best_model_weights)
 
-    result = one_iter(test_loader, 
+    result = one_iter(model, criterion, test_loader, 
                             device, 
                             train=False, 
                             monitoring_metrics=['accuracy', 'balanced_accuracy'])
---
+++ src/train_utils.py
@@ -0,0 +1,55 @@
+import numpy as np
+import sklearn.metrics as metrics
+
+def one_iter(model, criterion, loader, device, train=True, optimizer=None, scheduler=None, monitoring_metrics=list()):
+    running_loss = 0.0
+    count = 0.0
+    if train:
+        model.train()
+    else:
+        model.eval()
+
+    preds_arr = []
+    targets_arr = []
+    for data, labels in (loader):
+        batch_size = len(labels)
+        data = data.to(device)
+        labels = labels.to(device)
+        if optimizer is not None:
+            optimizer.zero_grad()
+        outputs = model(data)
+        loss = criterion(outputs, labels)
+        if train:
+            loss.backward()
+            optimizer.step()
+
+        preds = outputs.max(dim=1)[1]
+        count += batch_size
+        running_loss += loss.item() * batch_size
+
+        targets_arr.append(labels.cpu().numpy())
+        preds_arr.append(preds.detach().cpu().numpy())
+
+        if train and scheduler is not None:
+            scheduler.step()
+
+    targets = np.concatenate(targets_arr)
+    preds = np.concatenate(preds_arr)
+
+    loss = running_loss*1.0/count
+
+    result = {"loss": loss}
+    if "accuracy" in monitoring_metrics:
+        accuracy = metrics.accuracy_score(targets, preds)
+        result["accuracy"] = accuracy
+    if "balanced_accuracy" in monitoring_metrics:
+        balanced_accuracy = metrics.balanced_accuracy_score(targets, preds)
+        result["balanced_accuracy"] = balanced_accuracy
+    if "confusion_matrix" in monitoring_metrics:
+        cm = metrics.confusion_matrix(targets, preds)
+        result["confusion_matrix"] = cm
+    if "recall_per_class" in monitoring_metrics:
+        recall_per_class = metrics.recall_score(targets, preds, average=None)
+        result["recall_per_class"] = recall_per_class
+
+    return result
